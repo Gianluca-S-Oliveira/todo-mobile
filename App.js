aea97a109c3f0b05ac066876275202a135be400f
gianluca1453113 <gianluca@flyvet.app.br>
2024-05-15 17:48:44 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,134 @@
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  FlatList,
+  StyleSheet,
+} from "react-native";
+
+const App = () => {
+  const [name, setName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+  const handleAddTask = () => {
+    if (name && lastName) {
+      const newTask = `Nome da atividade ${name}:\nDescrição da atividade:${lastName}`;
+      setTasks([...tasks, newTask]);
+      setName("");
+      setLastName("");
+    }
+  };
+
+  const handleDeleteTask = (index) => {
+    const updatedTasks = [...tasks];
+    updatedTasks.splice(index, 1);
+    setTasks(updatedTasks);
+  };
+
+  const renderItem = ({ item, index }) => (
+    <View style={styles.task}>
+      <Text style={styles.itemList}>{item}</Text>
+      <TouchableOpacity onPress={() => handleDeleteTask(index)}>
+        <Text style={styles.deleteButton}>Apagar</Text>
+      </TouchableOpacity>
+    </View>
+  );
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.heading}>Registro de Afazeres</Text>
+      <Text style={styles.title}>Adicione e veja suas atividades!</Text>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="atividade"
+          value={name}
+          onChangeText={(text) => setName(text)}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Descricao"
+          value={lastName}
+          onChangeText={(text) => setLastName(text)}
+        />
+      </View>
+      <TouchableOpacity style={styles.addButton} onPress={handleAddTask}>
+        <Text style={styles.addButtonText}>Registrar Atividade</Text>
+      </TouchableOpacity>
+      <Text style={{ fontSize: "40px" }}>Atividades</Text>
+      <FlatList
+        data={tasks}
+        renderItem={renderItem}
+        keyExtractor={(item, index) => index.toString()}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 40,
+    marginTop: 40,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  heading: {
+    fontSize: 30,
+    fontWeight: "bold",
+    marginBottom: 7,
+    color: "green",
+  },
+  inputContainer: {
+    flexDirection: "column",
+    justifyContent: "space-between",
+    gap: 10,
+  },
+  input: {
+    marginTop: "10px",
+    marginTop: 10,
+    marginBottom: 10,
+    borderWidth: 3,
+    padding: 10,
+    borderRadius: 20,
+    borderColor: "green",
+  },
+  addButton: {
+    backgroundColor: "green",
+    padding: 10,
+    borderRadius: 5,
+    marginBottom: 10,
+  },
+  addButtonText: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+    fontSize: 18,
+  },
+  task: {
+    flexDirection: "column",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 15,
+    fontSize: 18,
+    backgroundColor: "#CEFFA8",
+    borderRadius: 12,
+    padding: 20,
+  },
+  itemList: {
+    fontSize: 19,
+  },
+  deleteButton: {
+    color: "red",
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+});
+
+export default App;
---
+++ README.md
@@ -0,0 +1,13 @@
+# Cadastro-Atividades-app
+
+Olá! Aqui é a versão aplicativo da minha aplicação web: https://cadastro-esg.vercel.app/
+nessa versão app, trouxe apenas a funcionalidade de adicionar e poder apagar uma tarefa, uma forma mais simplificada do projeto web, 
+mas que não deixa a desejar!
+
+Mais uma vez, para rodar o projeto basta seguir os passos:
+
+- git clone no projeto em questão em sua máquina
+- ter o aplicativo do expo go baixado em seu celular
+- rodar o comando npm install e após isso npx expo start para rodar
+- um QR code irá aparecer no terminal, basta no seu aplicativo do expo go ler esse QR CODE
+- se tudo for ok, o projeto ja deve aparecer!
